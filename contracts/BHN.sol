563436f4f55e779f92ae2d56eb09909267ab355a
unknown <bezalel3639@gmail.com>
2018-11-19 15:46:28 +0300

    Test init (one SOL only)

--- contracts/BHN.sol
+++ contracts/BHN.sol
@@ -1,5 +1,17 @@
 pragma solidity ^0.4.18;
 
 contract BHN {
-  
+  int private count = 0;
+
+  function increamentCounter () public {
+      count += 1;
+  }
+
+  function decreamentCounter () public {
+      count -= 1;
+  }
+
+  function getCount () public view returns {
+      return count;
+  }
 }
