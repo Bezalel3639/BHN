161cb2e4b0218d5fbd16a1a51cc6a6711643dbc8
unknown <bezalel3639@gmail.com>
2018-11-21 13:59:20 +0300

    Reading transaction passed

--- contracts/BHN.sol
+++ contracts/BHN.sol
@@ -1,7 +1,12 @@
-pragma solidity ^0.4.18;
+pragma solidity ^0.4.24;
+//pragma solidity ^0.4.22;
 
 contract BHN {
-  int private count = 0;
+  //int private count = 0;
+  int public count = 0;
+
+  //address public owner = msg.owner;
+  address public sender = msg.sender;
 
   function increamentCounter () public {
       count += 1;
@@ -11,7 +16,10 @@ contract BHN {
       count -= 1;
   }
 
-  function getCount () public view returns {
+  function getCount () public view returns (int) {
       return count;
+  }
+
+  function buy () public payable {
   }
 }
--- test/BHN.js
+++ test/BHN.js
@@ -1,7 +1,58 @@
 var BHN = artifacts.require("BHN");
 
-contract('BHN', function(accounts) {
+//contract('BHN', function(accounts) {
+contract('BHN', (accounts) => {
     it ("Test #1: configuration test", function() {
         assert.equal(8, 8);
     });
+
+  // let inst;
+  // beforeEach (async () => {
+  //     contract = await BHN.new()
+  //  })
+
+
+    it ("Test #2: transaction test", async () => {
+    // it ("Test #2", async function () {
+         let contract = await BHN.new ({from: accounts[1]});
+         //let contract = await BHN.new ();
+         let amount = web3.toWei(1, 'ether');
+         //let amount = 0;
+         let buyer = accounts [2];
+        //
+        let balanceBefore = web3.eth.getBalance(buyer);
+        let hash = await contract.buy.sendTransaction({from: buyer, value: amount});
+        let balanceAfter = web3.eth.getBalance(buyer);
+        //
+        let tx = await web3.eth.getTransaction(hash);
+        //console.log(msg.value); // error
+        console.log("Buyer :", buyer);
+        console.log("Amount sent (WEI): ", amount);
+        console.log("Balance before: ", balanceBefore.toNumber());
+        console.log("Balance after: ", balanceAfter.toNumber());
+        console.log("Nonce: " + tx.nonce);
+        console.log("Gas used: ", tx.gasUsed);
+        console.log("Gas: ", tx.gas);
+        console.log("Gas price: " + tx.gasPrice.toNumber());
+        console.log("Gas limit: " + tx.gasLimit);
+        console.log("To: " + tx.to);
+        console.log("Data: " + tx.data);
+        console.log("Origin: " + tx.origin);
+        //console.log("Transaction hash: ",  tx.transactionHash); // undefined
+        console.log("Transaction hash: ",  tx.hash); // OK
+        console.log("Block number: ",  tx.blockNumber); 
+        console.log("Transaction: ",  tx); // all in increamentCounter
+
+
+        let count = await contract.count.call();
+        console.log("Counter: ", count.toNumber());
+
+        //let owner = await contract.owner.call();
+        //console.log("Owner: ", owner);
+
+        let sender = await contract.sender.call();
+        console.log("Sender: ", sender);
+
+    });
+
 });
