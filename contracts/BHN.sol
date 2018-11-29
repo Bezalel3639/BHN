efa73d3b510f2d2d2634981bbb5d32d795353b8d
unknown <bezalel3639@gmail.com>
2018-11-29 10:20:50 +0300

    ERC20Interface added that conforms to the final standard

--- contracts/BHN.sol
+++ contracts/BHN.sol
@@ -1,6 +1,19 @@
 pragma solidity ^0.4.24;
 
-contract ERC20Token {
+// ERC20 standard: https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20.md
+contract ERC20Interface {
+    function totalSupply() public view returns (uint256);
+    function balanceOf(address _owner) public view returns (uint256);
+    function transfer(address _to, uint256 _value) public returns (bool success);
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
+    function approve(address _spender, uint256 _value) public returns (bool success);
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining);
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+}
+
+contract ERC20Token is ERC20Interface {
     string public teststring = "ERC20Token string";
 
     mapping (address => mapping (address => uint256)) public allowances;
@@ -49,24 +62,24 @@ contract ERC20Token {
 }
 
 contract BHN is ERC20Token {
-  int public count = 0;
-  address public sender = msg.sender;
+    int public count = 0;
+    address public sender = msg.sender;
 
-  //string public teststr = teststring;
-  //ERC20Token public token = new ERC20Token();
+    //string public teststr = teststring;
+    //ERC20Token public token = new ERC20Token();
 
-  function increamentCounter () public {
-      count += 1;
-  }
+    function increamentCounter () public {
+        count += 1;
+    }
 
-  function decreamentCounter () public {
-      count -= 1;
-  }
+    function decreamentCounter () public {
+        count -= 1;
+    }
 
-  function getCount () public view returns (int) {
-      return count;
-  }
+    function getCount () public view returns (int) {
+        return count;
+    }
 
-  function buy () public payable {
-  }
+    function buy () public payable {
+    }
 }
